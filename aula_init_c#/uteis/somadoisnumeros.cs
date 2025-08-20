7457c00ad5eb095056cf929616fe6ee8c4ea298d
Leopoldino005 <gabrielleopoldino005@gmail.com>
2025-08-20 20:46:30 -0300

    criação de classe

---
+++ aula_init_c#/uteis/somadoisnumeros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_init_c_.uteis
+{
+    public class somadoisnumeros
+    {
+        public int Somar(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+    }
+}
